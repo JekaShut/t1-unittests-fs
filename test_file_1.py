151f67847b237d843e633cfb709776d14d7b81ac
Eugene <e2.shut@qa-academy.by>
2020-07-16 13:14:15 +0300

    Initial

---
+++ test_file_1.py
@@ -0,0 +1,14 @@
+from file_1 import File
+
+content = "Hello world!"
+filename = "filename"
+
+
+def test_get_size():
+    test = File(filename, content).get_size()
+    assert test == len(content)
+
+def test_get_filename():
+    test = File(filename, content).get_filename()
+    assert test == filename
+
